ce7934befdf5185606a614d73ef2adeea1467991
Ridho Mulia <ridho050302@gmail.com>
2022-09-08 13:04:21 +0700

    Update views.py

--- wishlist/models.py
+++ wishlist/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 
 # Create your models here.
-from django.db import models
-
 class BarangWishlist(models.Model):
     nama_barang = models.CharField(max_length=50)
     harga_barang = models.IntegerField()
