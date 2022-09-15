46deac1383c8f2c0a94b29f9802d3c037e93b8d2
Ridho Mulia <ridho050302@gmail.com>
2022-09-15 13:04:13 +0700

    added serializer

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,13 +1,15 @@
+from django.http import HttpResponse
+from django.core import serializers
 from django.shortcuts import render
 from django.shortcuts import render
 from wishlist.models import BarangWishlist
 # Create your views here
 
 def show_wishlist(request):
-    data_barang_wishlist = BarangWishlist.objects.all()
+    data = BarangWishlist.objects.all()
     context = {
                 'list_barang': data_barang_wishlist,
                 'nama': 'Ridho Mulia'
                }
-    return render(request, "wishlist.html", context)
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
 
